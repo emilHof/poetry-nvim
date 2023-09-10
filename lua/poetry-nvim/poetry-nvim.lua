1a9579040ab2958fa1c27a59586d36ad92c7b018
Emil Hofstetter <emil.k.hofstetter@gmail.com>
2023-09-09 19:09:46 -0600

    Feat: make request for author

--- lua/poetry-nvim/poetry-nvim.lua
+++ lua/poetry-nvim/poetry-nvim.lua
@@ -1,9 +1,21 @@
+local lua_http = require("http")
+
 function buf_insert_poem()
 	local cursor = vim.api.nvim_win_get_cursor(0)
 	local row = cursor[1]
 	local col = cursor[2]
+end
 
-	print("Cursor is at row " .. row .. " and column " .. col)
+function get_poetry()
+	author = _get_rand_author();
+	print("author")
+end
+
+function _get_rand_author()
+	response, err = lua_http.request("GET", "https://poetrydb.org/author")
+	if err then error(err) end;
+
+	return response.body
 end
 
 vim.cmd([[command! Poet :lua buf_insert_poem()]])
